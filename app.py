8fe580ab04e361b5436de1829f8c948db4377032
loogle18 <sviat.minato@gmail.com>
2018-01-04 16:41:34 +0200

    Added wildcard routing for index. Added query_string to endpoint.

--- app.py
+++ app.py
@@ -8,19 +8,23 @@ app = Flask(__name__)
 
 PROXY_REQ_HEADERS_WHITELIST = ["content-type"]
 
-@app.route('/')
-def elastic():
-    auth = AWS4Auth(config.aws_access_key, 
-                    config.aws_secret_key, config.aws_region, 
+
+@app.route("/", defaults={'path': ''}, methods=["GET", "POST", "PUT"])
+@app.route('/<path:path>')
+def elastic(path):
+    auth = AWS4Auth(config.aws_access_key,
+                    config.aws_secret_key, config.aws_region,
                     config.aws_service)
-    endpoint = config.aws_endpoint + request.path
+    endpoint = config.aws_endpoint + request.path + "?" +\
+        request.query_string.decode("utf-8")
+    print(request.query_string.decode("utf-8"))
     proxy_header = {}
     for header in request.headers:
         if header[0].lower() in PROXY_REQ_HEADERS_WHITELIST:
             proxy_header[header] = request.headers[header]
 
     if request.method == "GET":
-        requests_response = requests.get(endpoint, auth=auth, 
+        requests_response = requests.get(endpoint, auth=auth,
                                          headers=proxy_header)
     return requests_response.content
 
