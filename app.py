ed3adee8af5e17070c380175576c98a8be7eaf86
loogle18 <sviat.minato@gmail.com>
2018-01-05 12:01:50 +0200

    Added POST and PUT methods.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import boto3
 import requests
 import config
+import json
 from flask import Flask, Response, request
 from requests_aws4auth import AWS4Auth
 
@@ -12,9 +13,10 @@ PROXY_RESP_HEADERS_BLACKLIST = ["connection", "content-length",
 
 
 @app.route("/", defaults={'path': ''}, methods=["GET", "POST", "PUT"])
-@app.route('/<path:path>')
+@app.route('/<path:path>', methods=["GET", "POST", "PUT"])
 def elastic(path):
-    proxy_header = {}
+    proxy_request_headers = {}
+    requests_response = None
     response = Response()
     auth = AWS4Auth(config.aws_access_key,
                     config.aws_secret_key, config.aws_region,
@@ -24,17 +26,34 @@ def elastic(path):
 
     for header in request.headers:
         if header[0].lower() in PROXY_REQ_HEADERS_WHITELIST:
-            proxy_header[header] = request.headers[header]
+            proxy_request_headers[header] = request.headers[header]
 
     if request.method == "GET":
         requests_response = requests.get(endpoint, auth=auth,
-                                         headers=proxy_header)
+                                         headers=proxy_request_headers)
+    elif request.method == "POST":
+        data = json.dumps(request.get_data().decode("utf-8"))
+        requests_response = requests.post(
+            endpoint,
+            auth=auth,
+            data=data,
+            headers=proxy_request_headers)
+    elif request.method == "PUT":
+        data = json.dumps(request.get_data().decode("utf-8"))
+        requests_response = requests.put(
+            endpoint,
+            auth=auth,
+            data=data,
+            headers=proxy_request_headers)
+    else:
+        return "Method is not allowed!"
 
     response.set_data(requests_response.content)
 
     for header in requests_response.headers:
         if header.lower() not in PROXY_RESP_HEADERS_BLACKLIST:
             response.headers[header] = requests_response.headers[header]
+
     return response
 
 
